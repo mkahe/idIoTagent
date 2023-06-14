ff3787487f0f0615155886af8aeadb0f5727cd4e
Amirhossein Ghaffari <ah.ghaffari94@gmail.com>
2023-06-14 16:35:56 +0300

    some minor edits

--- analyse.py
+++ analyse.py
@@ -8,21 +8,17 @@ print("temp max: ", get_max(temp))
 print("quality max: ", get_max(quality))
 print("light max: ", get_max(light))
 
-print("temp min: ", get_min(temp))
-print("quality min: ", get_min(quality))
-print("light min: ", get_min(light))
-
-print("temp avg: ", get_avg(temp))
-print("quality avg: ", get_avg(quality))
-print("light avg: ", get_avg(light))
+# print("temp min: ", get_min(temp))
+# print("quality min: ", get_min(quality))
+# print("light min: ", get_min(light))
 
 print("temp mean: ", get_mean(temp))
 print("quality mean: ", get_mean(quality))
 print("light mean: ", get_mean(light))
 
-print("temp var: ", get_var(temp))
-print("quality var: ", get_var(quality))
-print("light var: ", get_var(light))
+# print("temp var: ", get_var(temp))
+# print("quality var: ", get_var(quality))
+# print("light var: ", get_var(light))
 
 plot(temp, "Temperature")
 plot(quality, "Quality")
