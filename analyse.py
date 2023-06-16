f81caf18bd25eec57484432c326fa4862d6271ec
Mehrdad Kaheh <kahe.mehrdad@gmail.com>
2023-06-16 16:14:58 +0300

    some changes

--- analyse.py
+++ analyse.py
@@ -1,43 +1,105 @@
 from utils import *
+import time
 
-temp = get_value("temp")
-quality = get_value("quality")
-light = get_value("light")
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed time: {elapsed_time} seconds")
+        return result
+    return wrapper
 
-print(light)
+@timer
+def go_man_go():
+    temp = get_value("temp")
+    quality = get_value("quality")
+    light = get_value("light")
 
-print("temp max: ", get_max(temp))
-print("quality max: ", get_max(quality))
-print("light max: ", get_max(light))
+    # print(light)
 
-# print("temp min: ", get_min(temp))
-# print("quality min: ", get_min(quality))
-# print("light min: ", get_min(light))
+    # print("temp max: ", get_max(temp))
+    # print("quality max: ", get_max(quality))
+    # print("light max: ", get_max(light))
 
-print("temp mean: ", get_mean(temp, datetime.now() - timedelta(hours=21), datetime.now()))
-print("quality mean: ", get_mean(quality, datetime.now() - timedelta(hours=21), datetime.now()))
-print("light mean: ", get_mean(light))
+    # # print("temp min: ", get_min(temp))
+    # # print("quality min: ", get_min(quality))
+    # # print("light min: ", get_min(light))
 
-print("temp max in last hour: ", get_max(temp, datetime.now()-timedelta(hours=1), datetime.now()))
-print("temp min in last hour: ", get_min(temp, datetime.now()-timedelta(hours=1), datetime.now()))
-print("temp avg in last hour: ", get_mean(temp, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("temp mean: ", get_mean(temp, datetime.now() - timedelta(hours=21), datetime.now()))
+    # print("quality mean: ", get_mean(quality, datetime.now() - timedelta(hours=21), datetime.now()))
+    # print("light mean: ", get_mean(light))
 
-print("quality max in last hour: ", get_max(quality, datetime.now()-timedelta(hours=1), datetime.now()))
-print("quality min in last hour: ", get_min(quality, datetime.now()-timedelta(hours=1), datetime.now()))
-print("quality avg in last hour: ", get_mean(quality, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("temp max in last hour: ", get_max(temp, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("temp min in last hour: ", get_min(temp, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("temp avg in last hour: ", get_mean(temp, datetime.now()-timedelta(hours=1), datetime.now()))
 
-print("light max in last hour: ", get_max(light, datetime.now()-timedelta(days=1), datetime.now()))
-print("light min in last hour: ", get_min(light, datetime.now()-timedelta(days=1), datetime.now()))
-print("light avg in last hour: ", get_mean(light, datetime.now()-timedelta(days=1), datetime.now()))
+    # print("quality max in last hour: ", get_max(quality, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("quality min in last hour: ", get_min(quality, datetime.now()-timedelta(hours=1), datetime.now()))
+    # print("quality avg in last hour: ", get_mean(quality, datetime.now()-timedelta(hours=1), datetime.now()))
 
-print("light ci in last hour: ", get_ci(light, datetime.now()-timedelta(days=1), datetime.now()))
+    # print("light max in last hour: ", get_max(light, datetime.now()-timedelta(days=1), datetime.now()))
+    # print("light min in last hour: ", get_min(light, datetime.now()-timedelta(days=1), datetime.now()))
+    # print("light avg in last hour: ", get_mean(light, datetime.now()-timedelta(days=1), datetime.now()))
 
-print("temperature changes: ", temp_changes(temp))
+    # print("light ci in last hour: ", get_ci(light, datetime.now()-timedelta(days=1), datetime.now()))
 
-# print("temp var: ", get_var(temp))
-# print("quality var: ", get_var(quality))
-# print("light var: ", get_var(light))
+    # print("temperature changes: ", temp_changes(temp))
 
-# plot(temp, "Temperature")
-# plot(quality, "Quality")
-# plot(light, "light")
+    # print("light changes: ", light_changes(light))
+
+    # print("temp var: ", get_var(temp))
+    # print("quality var: ", get_var(quality))
+    # print("light var: ", get_var(light))
+
+    # plot(temp, "Temperature")
+    # plot(quality, "Quality")
+    # plot(light, "light")
+
+    # plot_series(temp, "Temperature")
+    # plot_series(light, "Light")
+    # plot_series(quality, "Quality")
+
+    start_date = datetime(2023, 6, 16, 10, 0)
+    first_hour = datetime(2023, 6, 16, 11, 0)
+    second_hour = datetime(2023, 6, 16, 12, 0)
+
+    print("TASK 1:")
+    print("   Temperature 1st hour")
+    print("      Mean ", round(get_mean(temp, start_date, first_hour), 3))
+    print(f"      CI [{round(get_ci(temp, start_date, first_hour)[0], 3)}, {round(get_ci(temp, start_date, first_hour)[1], 3)}]")
+    print("   Temperature 2nd hour")
+    print("      Mean ", round(get_mean(temp, first_hour, second_hour), 3))
+    print(f"      CI [{round(get_ci(temp, first_hour, second_hour)[0], 3)}, {round(get_ci(temp, first_hour, second_hour)[1], 3)}]")
+
+    print("TASK 2: Maximum Temperature = ", get_max(temp, start_date, second_hour))
+
+    print("TASK 4: Air quality:")
+    print("      Mean ", round(get_mean(quality, start_date, second_hour), 3))
+    print(f"      CI [{round(get_ci(quality, start_date, second_hour)[0], 3)}, {round(get_ci(quality, start_date, first_hour)[1], 3)}]")
+
+    print("TASK 5: Worst air quality ", get_max(quality, start_date, second_hour)) # TODO
+
+    print("TASK 6: Light ")
+    print("      Mean ", round(get_mean(light, start_date, second_hour), 3))
+    print(f"      CI [{round(get_ci(light, start_date, second_hour)[0], 3)}, {round(get_ci(light, start_date, first_hour)[1], 3)}]")
+
+    print(f"Total amoun of transmitted data is {round(get_data_size(start_date, second_hour), 2)} bytes")
+    # print(f"Total execution time of additional algorithms is {100} bytes")
+
+    print("\n\n")
+    print("light changes: ", light_changes(light))
+    print("\n\n")
+
+    print("quality changes: ", quality_changes(quality))
+
+    # plot_series(quality, "Quality")
+    # filtered = apply_filter(quality)
+    # plt.plot([k for k in range(len(filtered))],filtered)
+    # plt.show()
+
+    # classified = classify_quality(filtered)
+    # plt.plot([k for k in range(len(classified))],classified)
+    # plt.show()
+
+go_man_go()
--- utils.py
+++ utils.py
@@ -5,9 +5,10 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import scipy.stats as st
+from scipy.signal import savgol_filter
 
 def read_file():
-    with open('data_samples/idIoTagent_3.csv', 'r') as file:
+    with open('data_samples/idIoTagent_4.csv', 'r') as file:
         result = []
         csvreader = csv.reader(file)
         for row in csvreader:
@@ -21,8 +22,19 @@ def get_row(data, param):
     for row in data:
         if len(row) > 0:
             if row[2].strip() == param:
-                result.append((convert_to_date(row[5].strip(), row[7]), float(row[3].strip())))
+                result.append((convert_to_date(row[5].strip(), row[7]), float(row[3].strip())))    
     return result
+
+
+def get_data_size(from_date=None, to_date=None):
+    data = read_file()
+    result = []
+    for row in data:
+        if len(row) > 0:
+            result.append((convert_to_date(row[5].strip(), row[7]), float(row[9].strip())))
+    values = filter_data(result, from_date, to_date)
+    if len(values) == 0: return 0
+    return sum(values)
 
 
 def get_value(param):
@@ -83,14 +95,54 @@ def plot(data, param):
     plt.ylabel("Frequency")
     plt.title(f"{param} Histogram")
     plt.show()
-
+    
+def plot_series(data, param):
+    values = [temp for _, temp in data]
+    plt.title(param)
+    plt.plot([k for k in range(len(values))],values)
+    plt.show()
 
 def temp_changes(data):
+    return get_changes(data, 0.4)
+
+def light_changes(data):
+    return get_changes(data, 500)
+
+def quality_changes(data):
+    return get_changes(data, 100)
+
+def get_changes(data, thresh):
     dramatic_changes = []
     for i in range(1, len(data)):
-        current_datetime, current_temp = data[i]
-        prev_datetime, prev_temp = data[i-1]
-        temp_difference = abs(current_temp - prev_temp)
-        if temp_difference >= 1.0:
+        current_datetime, current_value = data[i]
+        prev_datetime, prev_value = data[i-1]
+        temp_difference = abs(current_value - prev_value)
+        if temp_difference >= thresh:
             dramatic_changes.append(current_datetime)
     return dramatic_changes
+
+def apply_filter(data):
+    values = [value for _, value in data]
+    y_filtered = savgol_filter(values, window_length = 200, polyorder = 2)
+    return y_filtered
+
+def classify_quality(data, from_date=None, to_date=None):
+    new = []
+    thresh = np.mean(data)
+    max = np.max(data)
+    min = np.min(data)
+    for row in data:
+        if row < thresh:
+            new.append(min)
+        if row >= thresh:
+            new.append(max)
+    return new
+
+def quality_changes(data):
+    filtered = apply_filter(data)
+    classified = classify_quality(filtered)
+    result = []
+    for i in range(len(data)):
+        result.append((data[i][0], classified[i]))
+    return get_changes(result, 100)
+    
